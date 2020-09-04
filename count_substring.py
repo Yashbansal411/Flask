bdcabd246ee7b9167bcb8059403def66dd3e01ec
Yashbansal411 <38386847+Yashbansal411@users.noreply.github.com>
2020-09-04 11:13:46 +0530

    Add files via upload

---
+++ count_substring.py
@@ -0,0 +1,30 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'hello_world'
+@app.route('/count/')
+def count_sub():
+    file = open('read.txt','r')
+    string = file.read()
+    substring = 'ubuntu'
+    count = 0
+    start = 0
+    print(string)
+    while start < len(string):
+        pos = string.find(substring,start)
+        if pos == -1:
+            break
+        else:
+            start = pos +1
+            count = count+1
+
+    ans = {"string": string,
+           "sub-string": substring,
+           "count": count
+           }
+    return ans
+
+if __name__ == "__main__":
+    app.run(debug = True)
